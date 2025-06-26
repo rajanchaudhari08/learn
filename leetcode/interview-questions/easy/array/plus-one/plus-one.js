459e0836d8e10e2937ce351045324befdcedfc84
rajanchaudhari08 <rajanchaudharigits@gmail.com>
2025-06-26 12:36:39 +0530

    :memo: Add - Leetcode: Plus One (Documentation)

---
+++ leetcode/interview-questions/easy/array/plus-one/plus-one.js
@@ -0,0 +1,27 @@
+/**
+ * Increments the large integer represented by an array of digits.
+ * @param {number[]} digits - The input array of digits.
+ * @returns {number[]} - The incremented array of digits.
+ */
+
+const plusOne = (digits) => {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+  return digits;
+};
+
+console.log(plusOne([1, 2, 3]));
+
+/**
+ * Results: [1, 2, 4]
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
