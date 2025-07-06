b5e7861d63f03aa202705e553820e462d3495671
rajanchaudhari08 <rajanchaudharigits@gmail.com>
2025-07-06 19:19:24 +0530

    :memo: Add - Leetcode: Lower Case (Documentation)

---
+++ leetcode/interview-questions/easy/string/lower-case/lower-case.js
@@ -0,0 +1,15 @@
+/**
+ * Transforms every uppercase character in the input string to lowercase.
+ * @param {string} inputString - The string to convert.
+ * @returns {string} - The lowercase version of the input.
+ */
+
+const toLowerCase = (inputString) => {
+  return inputString.toLowerCase();
+};
+
+console.log(toLowerCase("Javascript"));
+
+/**
+ * Results: javascript
+ */
---
+++ leetcode/interview-questions/easy/string/lower-case/lower-case.md
@@ -0,0 +1,5 @@
+# [To Lower Case](https://leetcode.com/problems/to-lower-case/description/)
+
+**Problem Statement**
+
+_Given a string s, return the string after replacing every uppercase letter with the same lowercase letter._
